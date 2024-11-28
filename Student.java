4627c002804f90d7bb758221c0fdcc225445d2f3
RanjithKumar200210 <ranjithsangeetha10@gmail.com>
2024-11-28 13:09:40 +0530

    day5part2

---
+++ Student.java
@@ -0,0 +1,32 @@
+package Day5;
+
+public class Student {
+	String Name="";
+	int age=0;
+	String dateOfBirth="";
+	int rollNo=0;
+	String character="";
+	
+	char grade;
+	public static void main(String[] args) {
+		Student s1=new Student();
+		s1.Name="Ranjith kumar B";
+		s1.age=22;
+		s1.dateOfBirth="10/09/2002";
+		s1.rollNo=1274;
+		s1.character="VERY GOOD";
+		s1.grade='A';
+		System.out.println("student name:"+s1.Name);
+		System.out.println("age:"+s1.age);
+		System.out.println("date of birth:"+s1.dateOfBirth);
+		System.out.println("rollno:"+s1.rollNo);
+		System.out.println("Character:"+s1.character);
+		System.out.println("GRADE:"+s1.grade);
+	
+		
+		
+	}
+	
+	}
+
+
