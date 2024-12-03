289ea2722e8f2a4e2a444c5c0fd6f740ea68d29c
RanjithKumar200210 <ranjithsangeetha10@gmail.com>
2024-12-03 12:57:12 +0530

    day7

---
+++ Day7/patternPrinting.java
@@ -0,0 +1,13 @@
+package Day7;
+
+public class patternPrinting {
+	public static void main(String[]args) {
+		for(int i=0;i<=3;i++) {
+			for(int j=0;j<=3;j++){
+				System.out.print("*\t");
+			}
+			System.out.println();
+		}
+		}
+	}
+
---
+++ Day7/varanamAyiram.java
@@ -0,0 +1,14 @@
+package Day7;
+
+public class varanamAyiram {
+	public static void main(String[]args) {
+		String name="dead";
+		String str=new String("dead");
+		if(name.equals(str)) {
+			System.out.println("surya weds megna");
+		}else {
+			System.out.println("surya weds ramya ");
+		}
+	}
+
+}
